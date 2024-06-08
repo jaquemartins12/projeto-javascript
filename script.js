6151d449fcd2b8a4db9234c9cd3c94f278230daa
Jaque Martins <jaquelinerisa@gmail.com>
2024-06-07 21:15:30 -0300

    update
    Aula dia 07/06

--- script.js
+++ script.js
@@ -5,5 +5,38 @@ function changeImage () {
 
     if (currentSrc === 'imagens/Dom.JPEG') {
         imagem.setAttribute ('src', 'imagens/Mia.jpeg');
+
+        imagem.setAttribute ('alt', 'Mia Toretto');
+    } else {
+        imagem.setAttribute ('src', "imagens/Bryan.jpeg")
+    }
+}
+
+function trocarImagem () {
+    const imagemDois = document.getElementById ('segundaImagem');
+
+    const currentSrc2 = imagemDois.getAttribute ('src');
+
+    if (currentSrc2 === 'imagens/Mia.jpeg') {
+        imagemDois.setAttribute ('src', 'imagens/Bryan.jpeg');
+
+        imagemDois.setAttribute ('alt', 'Bryan');
+    } else {
+        imagemDois.setAttribute ('src', 'imagens/Dom.JPEG');
+    }
+}
+
+function trocarImagem3 () {
+    const terceiraImagem = document.getElementById ('terceiraImagem');
+
+    const currentSrc3 = terceiraImagem.getAttribute ('src');
+
+    if (currentSrc3 === 'imagens/Bryan.jpeg') {
+        terceiraImagem.setAttribute ('src', 'imagens/Dom.JPEG');
+
+        terceiraImagem.setAttribute ('alt', 'Bryan');
+    } else {
+        terceiraImagem.setAttribute ('src', 'imagens/Mia.jpeg');
+        terceiraImagem.setAttribute ('alt', 'Dom');
     }
 }
